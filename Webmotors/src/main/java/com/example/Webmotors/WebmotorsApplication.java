bff65d07a8143aecf02ffab406b91ba6a83f9f94
Luiz Felipe Rodrigues <luizbr080604@gmail.com>
2024-06-19 14:29:05 -0300

    Add files via upload

---
+++ Webmotors/src/main/java/com/example/Webmotors/WebmotorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Webmotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmotorsApplication.class, args);
+	}
+
+}
---
+++ Webmotors/target/classes/db/migration/V1__CreateTable.sql
@@ -0,0 +1,4 @@
+CREATE TABLE brand (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
